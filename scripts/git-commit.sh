7b4255b3e04e0c8898e3d936719537e74524aaba
8kto <ifhtagn@gmail.com>
2026-07-02 00:10:54 +0200

    Update commit script

--- scripts/git-commit.sh
+++ scripts/git-commit.sh
@@ -13,22 +13,16 @@ $gc src/markdown/\$* -m 'Update notes and non-reader texts'
 $gc src/markdown/*appendix* -m 'Update appendices'
 
 $ga src/markdown/*.md
-#git reset 0800-bestiary--autogen.md
 $gc -m 'Extend rooms'
 
 $gc src/styles/*.css -m 'Update styles'
 $gc src/html/* -m 'Update layout files'
 
-#$gc Conventions.md -m 'Extend conventions'
+$gc Conventions.md -m 'Extend conventions'
 $gc scripts/git-commit.sh -m 'Update commit script'
-$gc scripts/**/*.mjs -m 'Update build scripts'
 $gc scripts/**/*.sh -m 'Update project scripts'
 
-#$gc conf/*build.conf.mjs -m 'Update module build config'
-#$gc conf/*toc.conf.mjs -m 'Update TOC overrides'
-#$gc Readme.md -m 'Update readme file'
-
-#$ga maps
-#$gc -m 'Extend maps'
+$gc conf/*build.conf.mjs -m 'Update module build config'
+$gc Readme.md -m 'Update readme file'
 
 exit 0
